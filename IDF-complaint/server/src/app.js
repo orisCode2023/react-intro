f2e955779c3cf462856b962c0264b623ab4f3c95
odmark2023@gmail.com <odmark2023@gmail.com>
2026-02-18 22:48:10 +0200

    fetching complete

--- IDF-complaint/server/src/app.js
+++ IDF-complaint/server/src/app.js
@@ -3,14 +3,14 @@ import routerComplaint from './routes/complaints.routes.js';
 import routerPassword from './routes/password.routes.js';
 import connectDB from '../src/db/connect.js'
 import protect from './middlewares/requireAdminAuth.js';
-
+import cors from 'cors'
 await connectDB()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-
+app.use(cors())
 app.use('/complaint', routerComplaint)
 app.use('/getComplaints', protect, routerComplaint)
 app.use('/password',  routerPassword)
